1d7e985a82914c93392f2e23a913b27a06a9ce7d
Vikas <167329026+Vikaskumar242004@users.noreply.github.com>
2025-10-01 16:53:51 +0530

    adding folder 2

---
+++ video 2/main.js
@@ -0,0 +1,26 @@
+// const http = require('node:http');
+// const fs = require("fs")  // isse(require) hum module ko import krte he and this is also known as common.js 
+//  or agar hum require ka use kr rhe he to hum package me type module nahi kr skte 
+
+                                    // or
+                        
+// import http from "http"   // thjs is Es.js iske liye hume package.json me type module dena pdega
+
+
+
+
+
+// this is named export 
+// import {a} from "./my.js"
+// console.log(a) 
+
+                    //   or
+
+// this is default export  
+import obj from "./my.js"
+console.log(obj)
+
+
+         // part of you.js
+// const a = require("./you.js")
+// console.log(a)
---
+++ video 2/my.js
@@ -0,0 +1,12 @@
+export const a = 1   // named export because it exported by its name
+export const b = 2
+export const c = 4
+export const d = 3
+
+
+// default export 
+let obj;
+ export default obj = {
+    x : 5,
+    y : 7
+ }
